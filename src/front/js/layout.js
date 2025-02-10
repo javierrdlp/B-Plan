b3390b410f5cb3cf60e569a44a8e4713d16cc447
Jose Miguel <josemi_garcia_seven@hotmail.com>
2025-02-10 12:05:38 +0000

    Ajuste el navbar, se muestra en todos los componentes menos en el componente Home (pagina de Login).

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom"; 
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 
@@ -13,23 +13,12 @@ import { ActivePlans } from "./pages/activePlans";
 import NewPlan from './pages/newPlan';
 import JoinPlan from './pages/joinPlan';  
 
-
- 
-
-
-
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
-
-//create your first component
 const Layout = () => {
-    //the basename is used when your project is published in a subdirectory and not in the root of the domain
-    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-    const basename = process.env.BASENAME || "";
-
-    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
     const [token, setToken] = useState(localStorage.getItem("token"));
+    
     useEffect(() => {        
         const updateToken = () => {
           setToken(localStorage.getItem("token"));
@@ -40,29 +29,21 @@ const Layout = () => {
         return () => {
           window.removeEventListener("storage", updateToken);
         };
-      }, []);
+    }, []);
+
+    
+    const location = useLocation();
+    
+    
+    const isHomePage = location.pathname === "/";
 
     if(localStorage.getItem("token")){  
-        
-        const planData = {
-            name: "Visit to the Museum",
-            numberOfPeople: 5,
-            date: "2025-02-15",
-            startTime: "10:00",
-            endTime: "13:00",
-            location: "Museum of Modern Art",
-            category: "Art",
-            description: "Join us for a cultural experience at the museum.",
-            image: "https://cdn-museabrugge-be.cloud.glue.be/https%3A%2F%2Fwww.museabrugge.be%2Fvolumes%2Fgeneral%2FBezoek-het-Groeningemuseum_Musea-Brugge.jpg?dpr=2&w=1440&h=590&fit=crop&s=3c676338b7222eaf5b274e130e09698e", 
-            locationCoordinates: { lat: 40.4168, lng: -3.7038 },
-        };
-        
-        
-    return (
-        <div>
-            <BrowserRouter basename={basename}>
+        return (
+            <div>
+                
                 <ScrollToTop>
-                    <Navbar />
+                    
+                    {!isHomePage && <Navbar />}
                     <Routes>
                         <Route element={<Home />} path="/" />
                         <Route element={<Profile />} path="/profile" /> 
@@ -76,32 +57,37 @@ const Layout = () => {
                     </Routes>
                     <Footer />
                 </ScrollToTop>
-            </BrowserRouter>
-        </div>
-    );
-    }else{
+            </div>
+        );
+    } else {
         return (
             <div>
-                <BrowserRouter basename={basename}>
-                    <ScrollToTop> 
-                    <Navbar />                       
-                        <Routes>
-                            <Route element={<Home />} path="/" />
-                            <Route element={<Profile />} path="/profile" /> 
-                            <Route path="/plans-history" element={<PlansHistory />} />
-                            <Route path="/active-plans" element={<ActivePlans />} />
-                            <Route path="/new-plan" element={<NewPlan />} />
-                            <Route path="/join-plan" element={<JoinPlan />} />
-                            <Route element={<Demo />} path="/demo" />
-                            <Route element={<Single />} path="/single/:theid" />
-                            <Route element={<h1>Not found!</h1>} />
-                        </Routes>
-                        <Footer />
-                    </ScrollToTop>
-                </BrowserRouter>
+                <ScrollToTop> 
+                    
+                    {!isHomePage && <Navbar />}
+                    <Routes>
+                        <Route element={<Home />} path="/" />
+                        <Route element={<Profile />} path="/profile" /> 
+                        <Route path="/plans-history" element={<PlansHistory />} />
+                        <Route path="/active-plans" element={<ActivePlans />} />
+                        <Route path="/new-plan" element={<NewPlan />} />
+                        <Route path="/join-plan" element={<JoinPlan />} />
+                        <Route element={<Demo />} path="/demo" />
+                        <Route element={<Single />} path="/single/:theid" />
+                        <Route element={<h1>Not found!</h1>} />
+                    </Routes>
+                    <Footer />
+                </ScrollToTop>
             </div>
         );
     }
 };
 
-export default injectContext(Layout);
+
+const App = () => (
+    <BrowserRouter>
+        <Layout />
+    </BrowserRouter>
+);
+
+export default injectContext(App);
